49b8a62a951366b37168404e0a0ee07f1640ad91
hdelanoe <27762445+hdelanoe@users.noreply.github.com>
2024-08-07 01:31:58 +0200

    prepare for railway

--- src/home/settings.py
+++ src/home/settings.py
@@ -25,7 +25,14 @@ SECRET_KEY = 'django-insecure-p86-wl*_c11x$a!--*&emrjc4ne=7l*2e4-&)qj2@r7d2x9-%&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.railway.app'
+]
+if DEBUG:
+    ALLOWED_HOSTS += [
+        '127.0.0.1',
+        'localhost',
+    ]
 
 
 # Application definition
--- src/home/views.py
+++ src/home/views.py
@@ -6,10 +6,23 @@ from visits.models import PageVisit
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = qs.filter(path=request.path)
     my_title = "My title"
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+
+    }
+    html_template = "home.html"
+    PageVisit.objects.create(path=request.path)
+    return render(request, html_template, my_context)
+
+def about_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = qs.filter(path=request.path)
+    my_title = "About title"
     my_context = {
         "page_title": my_title,
         "page_visit_count": page_qs.count(),
