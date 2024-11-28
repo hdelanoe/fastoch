ccc22c310e0486243de53eb25c7de9e34ac58af7
hdelanoe <delanoe.hugo@gmail.com>
2024-11-28 17:21:54 +0100

    ready to dev waiting list

--- src/dashboard/urls.py
+++ src/dashboard/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import dashboard_view, create_inventory
+from .views import dashboard_view, create_inventory, add_product_from_photo
 
 urlpatterns = [
     path('', dashboard_view, name='dashboard'),
     path('create_inventory/', create_inventory),
+    path('add_product_from_photo/', add_product_from_photo, name='add_product_from_photo'),
 ]    
--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -10,7 +10,9 @@ from home.views import init_context
 def dashboard_view(request):
     context = init_context()
     if not context["inventory_list"] :
-        return render(request, "dashboard/dashboard_new_inventory.html", context) 
+        return render(request, "dashboard/dashboard_new_inventory.html", context)
+    inventory = Inventory.objects.get(is_current=True)
+    context["inventory"] = inventory
     return render(request, "dashboard/dashboard.html", context) 
 
 @login_required
@@ -27,4 +29,9 @@ def create_inventory(request):
             messages.success(request, "Your inventory has been created.")
         except Inventory.DoesNotExist:
             messages.error(request, "Error while create your inventory.")
+    return redirect(reverse("dashboard"))
+
+@login_required
+def add_product_from_photo(request):
+    messages.warning(request, "Feature en dev.")
     return redirect(reverse("dashboard"))
